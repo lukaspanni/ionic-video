dd0001518667c44c070400c55e08cd32bcf7e90f
Lukas Panni <lukas.panni@outlook.de>
2022-11-17 15:57:16 +0100

    Add camera plugin

--- ionic-video/src/app/home/home.page.ts
+++ ionic-video/src/app/home/home.page.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-
+import {
+  CameraPreview,
+  CameraPreviewPictureOptions,
+  CameraPreviewOptions,
+  CameraPreviewDimensions,
+} from '@awesome-cordova-plugins/camera-preview/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private camera: CameraPreview) {}
 
-  constructor() {}
+  startCamera() {
+    // camera options (Size and location). In the following example, the preview uses the rear camera and display the preview in the back of the webview
+    const cameraPreviewOpts: CameraPreviewOptions = {
+      x: 0,
+      y: 0,
+      width: window.screen.width,
+      height: window.screen.height,
+      camera: 'rear',
+      tapPhoto: true,
+      previewDrag: true,
+      toBack: true,
+      alpha: 1,
+      storeToFile: true,
+    };
 
+    // start camera
+    this.camera.startCamera(cameraPreviewOpts).then(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
